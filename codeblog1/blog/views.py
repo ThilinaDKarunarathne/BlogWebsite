4b3d5c4d9f00367ad424adf3fdb74f1b513917b0
Thilina Dhanajaya <adamsampath2002@gmail.com>
2024-12-16 12:13:58 +0530

    pages created

--- codeblog1/blog/views.py
+++ codeblog1/blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+
+def bloghome (index):
+    return HttpResponse("This is a blog Home page")
+
+def blogpost (request,slug):
+    return HttpResponse(f'This is the blog post : {slug}')
